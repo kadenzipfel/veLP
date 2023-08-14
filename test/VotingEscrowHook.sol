d0aea527d7a72269f550e4011e4e7049d01edf5f
kadenzipfel <kaden.zipfel@hotmail.com>
2023-08-14 15:18:39 -0700

    Setup test suite

---
+++ test/VotingEscrowHook.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {Deployers} from "@uniswap/v4-core/test/foundry-tests/utils/Deployers.sol";
+import {TestERC20} from "@uniswap/v4-core/contracts/test/TestERC20.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {PoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {VotingEscrowHook} from "../src/VotingEscrowHook.sol";
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+
+contract VotingEscrowHookTest is Test, Deployers {
+    using PoolIdLibrary for PoolKey;
+
+    TestERC20 token0;
+    TestERC20 token1;
+    PoolManager manager;
+    VotingEscrowHookImplementation votingEscrowHook = VotingEscrowHookImplementation(address(uint160(
+        Hooks.BEFORE_MODIFY_POSITION_FLAG | Hooks.AFTER_MODIFY_POSITION_FLAG
+    )));
+    PoolKey key;
+    PoolId id;
+
+    PoolModifyPositionTest modifyPositionRouter;
+
+    function setUp() public {
+        token0 = new TestERC20(2**128);
+        token1 = new TestERC20(2**128);
+        manager = new PoolManager(500000);
+
+        vm.record();
+        VotingEscrowHook impl = new VotingEscrowHookImplementation(manager, token0, "veToken", "veTKN", votingEscrowHook);
+        (, bytes32[] memory writes) = vm.accesses(address(impl));
+        vm.etch(address(votingEscrowHook), address(impl).code);
+        // for each storage key that was written during the hook implementation, copy the value over
+        unchecked {
+            for (uint256 i = 0; i < writes.length; i++) {
+                bytes32 slot = writes[i];
+                vm.store(address(votingEscrowHook), slot, vm.load(address(impl), slot));
+            }
+        }
+        key =
+            PoolKey(Currency.wrap(address(token0)), Currency.wrap(address(token1)), 0, MAX_TICK_SPACING, votingEscrowHook);
+        id = key.toId();
+        votingEscrowHook.setPoolId(id);
+
+        modifyPositionRouter = new PoolModifyPositionTest(manager);
+
+        token0.approve(address(votingEscrowHook), type(uint256).max);
+        token1.approve(address(votingEscrowHook), type(uint256).max);
+        token0.approve(address(modifyPositionRouter), type(uint256).max);
+        token1.approve(address(modifyPositionRouter), type(uint256).max);
+    }
+}
---
+++ test/VotingEscrowHookImplementation.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {BaseHook} from "../../../contracts/BaseHook.sol";
+import {VotingEscrowHook} from "../src/VotingEscrowHook.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+
+contract VotingEscrowHookImplementation is VotingEscrowHook {
+    constructor(IPoolManager _poolManager, address _token, string memory _name, string memory _symbol, VotingEscrowHook addressToEtch) VotingEscrowHook(_poolManager, _token, _name, _symbol) {
+        Hooks.validateHookAddress(addressToEtch, getHooksCalls());
+    }
+
+    // make this a no-op in testing
+    function validateHookAddress(BaseHook _this) internal pure override {}
+}
